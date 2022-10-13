0c63e13d329bab9c9022fc9521c9bf930125b194
Peter Corke <peter.i.corke@gmail.com>
2022-10-13 18:41:55 +1100

    update these low level functions to toolbox standards

--- toolbox/isrot.m
+++ toolbox/isrot.m
@@ -1,39 +1,24 @@
-%ISROT Test if SO(3) rotation matrix
+%ISROT Test if value is SO(3) rotation matrix
 %
 % ISROT(R) is true (1) if the argument is of dimension 3x3 or 3x3xN, else false (0).
 %
-% ISROT(R, 'check') as above, but also checks the validity of the rotation
+% ISROT(R, check=true) as above, but also checks the validity of the rotation
 % matrix.
 %
 % Notes::
-% - A valid rotation matrix has determinant of 1.
+% - A valid rotation sub-matrix R has R'*R = I and det(R)=1.
+% - The first form is a fast, but incomplete, test that a transform belong
+%   to SO(3).
 %
 % See also ISHOMOG, ISROTM2, ISVEC.
 
-% Copyright (C) 1993-2019 Peter I. Corke
-%
-% This file is part of The Spatial Math Toolbox for MATLAB (SMTB).
-%
-% Permission is hereby granted, free of charge, to any person obtaining a copy
-% of this software and associated documentation files (the "Software"), to deal
-% in the Software without restriction, including without limitation the rights
-% to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
-% of the Software, and to permit persons to whom the Software is furnished to do
-% so, subject to the following conditions:
-%
-% The above copyright notice and this permission notice shall be included in all
-% copies or substantial portions of the Software.
-%
-% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
-% FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
-% COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
-% IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-% CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-%
-% https://github.com/petercorke/spatial-math
+% Copyright 2022-2023 Peter Corke, Witold Jachimczyk, Remo Pillat 
 
-function h = isrot(R, ~)
+function h = isrot(R, options)
+    arguments
+        R double
+        options.check (1,1) logical = false
+    end
 
 h = false;
 d = size(R);
@@ -46,10 +31,12 @@ if ndims(R) >= 2
     if nargin > 1
         for i = 1:size(R,3)
             RR = R(:,:,i);
+            % check transpose is inverse
             e = RR'*RR - eye(3,3);
             if norm(e) > 10*eps
                 return %false
             end
+            % check determinant is +1
             e = abs(det(RR) - 1);
             if norm(e) > 10*eps
                 return %false
