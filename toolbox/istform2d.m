791eca217c8d48a35c479fa8b4bf47a744c687d5
Peter Corke <peter.i.corke@gmail.com>
2022-10-14 15:25:17 +1100

    rename these predicates to match toolbox convention, update code to suit return false if pose object

--- toolbox/istform2d.m
+++ toolbox/istform2d.m
@@ -1,43 +1,45 @@
-%ISHOMOG Test if value is SE(3) homogeneous transformation matrix
+%ISTFORM2D Test if value is SE(2) homogeneous transformation matrix
 %
-% ISHOMOG(T) is true (1) if the argument T is of dimension 4x4 or 4x4xN,
+% ISTFORM2D(T) is true (1) if the argument T is of dimension 3x3 or 3x3xN,
 % else false (0).
 %
-% ISHOMOG(T, check=true) as above, but also checks the validity of the
+% ISTFORM2D(T, check=true) as above, but also checks the validity of the
 % rotation sub-matrix.
 %
 % Notes:
 % - A valid rotation sub-matrix R has R'*R = I and det(R)=1.
-% - The first form is a fast, but incomplete, test that a matrix belongs 
-%   to SE(3).
+% - The first form is a fast, but incomplete, test that a matrix belong
+%   to SE(2).
 %
-% See also ISROT, ISHOMOG2D, ISVEC.
+% See also ISTFORM, ISROTM2D, ISVEC.
 
 % Copyright 2022-2023 Peter Corke, Witold Jachimczyk, Remo Pillat 
 
-
-function h = ishomog(T, options)
+function h = istform2d(T, options)
     arguments
-        T double
+        T
         options.check (1,1) logical = false
     end
 
     h = false;
+    if ~isnumeric(T)
+        return
+    end
+
     dims = size(T);
-    
     if ndims(T) >= 2
-        % first two dimensions must be 4x4
-        if ~(all(dims(1:2) == [4 4]))
+        % first two dimensions must be 3x3
+        if ~(all(dims(1:2) == [3 3]))
             return %false
         end
-        
+       
         if options.check
             % each plane must contain a valid rotation matrix
             for i = 1:size(T,3)
                 % check rotational part
-                R = T(1:3,1:3,i);
+                R = T(1:2,1:2,i);
                 % check transpose is inverse
-                e = R'*R - eye(3,3);
+                e = R'*R - eye(2,2);
                 if norm(e) > 10*eps
                     return %false
                 end
@@ -47,12 +49,11 @@ function h = ishomog(T, options)
                     return %false
                 end
                 % check bottom row
-                if ~all(T(4,:,i) == [0 0 0 1])
+                if ~all(T(3,:,i) == [0 0 1])
                     return %false
                 end
             end
         end
     end
     h = true;
-
 end
