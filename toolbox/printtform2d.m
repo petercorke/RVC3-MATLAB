2325e8e7b9ef7e4499f8eccfa9e1ba12d32962c2
Peter Corke <peter.i.corke@gmail.com>
2022-10-14 16:04:40 +1100

    update doco and argument parsing

--- toolbox/printtform2d.m
+++ toolbox/printtform2d.m
@@ -1,111 +1,112 @@
 %PRINTTFORM2D Compact display of SE(2) homogeneous transformation
 %
-% PRINTTFORM2D(T, OPTIONS) displays the homogoneous transform (4x4) in a compact
-% single-line format.  If T is a homogeneous transform sequence then each
-% element is printed on a separate line.  T can also be an object of type
-% se3, rigidtform3d or Twist.
+% PRINTTFORM(T) displays pose in a compact single-line format. Pose is
+% given as 3x3 homogoneous transform or as an se2, rigidtform2d or Twist2d
+% object.  If T is a sequence, a 3x3xN matrix or a vector of objects, then
+% each element is printed on a separate line.
 %
-% PRINTTFORM2D(R, OPTIONS) as above but displays the SO(3) rotation matrix (3x3).
-% R can also be an object of type so3.
+% PRINTTFORM(R) as above but displays a 2D rotation expressed as a 2x2
+% rotation matrix, or as an so2 object.
 %
-% S = PRINTTFORM2D(T, OPTIONS) as above but returns the string.
+% S = PRINTTFORM(...) as above but returns the string for a single pose
+% only.
 %
-% >> PRINTTFORM2D T OPTIONS is the command line form of above.
+% Options:
+% 
+% unit   - Rotational units "rad" (default) or "deg"
+% fmt    - Format string for all numbers, (default "%.2g" or "%8.2g" for
+%          the sequence case)
+% label  - Label text to display to the left of the pose.  If not given,
+%          and a variable is passed then the variable name is used, use
+%          label="" to suppress.
+% fid    - File identifier to write string to (defaults to stdout)
 %
-% Options::
-% 'radian'     display angle in radians (default is degrees)
-% 'fmt', f     use format string f for all numbers, (default %g)
-% 'label',l    display the text before the transform
-% 'fid',F      send text to the file with file identifier F
 %
-% Examples::
-%        >> trprint(T2)
-%        t = (0,0,0), RPY/zyx = (-122.704,65.4084,-8.11266) deg
-%
-%        >> trprint(T1, 'label', 'A')
-%               A:t = (0,0,0), RPY/zyx = (-0,0,-0) deg
-%
-%       >> trprint B euler
-%       t = (0.629, 0.812, -0.746), EUL = (125, 71.5, 85.7) deg
-%
-% Notes::
-% - If the 'rpy' option is selected, then the particular angle sequence can be
-%   specified with the options 'xyz' or 'yxz' which are passed through to TR2RPY.
-%  'zyx' is the default.
-%
-% See also TR2EUL, TR2RPY, TR2ANGVEC.
+% See also PRINTTFORM.
 
-%## 3d homogeneous
+% Copyright 2022-2023 Peter Corke, Witold Jachimczyk, Remo Pillat 
 
-% Copyright (C) 1993-2019 Peter I. Corke
+function out = printtform2d(X, options)
+    arguments
+        X
+        options.fmt (1,1) string = ""
+        options.unit (1,1) string {mustBeMember(options.unit, ["rad", "deg"])} = "rad"
+        options.fid (1,1) {mustBeInteger} = 1
+        options.label (1,1)
+    end
 
-
-function out = printtform2d(X, varargin)
-
-    opt.fmt = [];
-    opt.mode = {'rpy', 'euler', 'axang'};
-    opt.unit = 'rad';
-    opt.label = [];
-    opt.fid = 1;
-
-
-    opt.dim = 2;
-
-
-    [opt,args] = tb_optparse(opt, varargin);
-
-    if isa(X,'se2')
+    if istform2d(X)
+        T = X;
+    elseif isrotm2d(X)
+        T = X;
+    elseif isa(X,'se2')
         X = X.tform;
     elseif isa(X,'so2')
         X = X.rotm;        
     elseif isa(X, 'Twist2')
         X = X.tform;
-    end
-
-% Do not print the variable name. It's not very useful.
-    if isempty(opt.label)
-        opt.label = inputname(1);
-    end
-
-    s = '';
-
-    if size(X,3) == 1
-        if isempty(opt.fmt)
-            opt.fmt = '%.3g';
-        end
-        s = tr2s2d(X, opt, args{:});
-
+    elseif isa(X, "rigidtform2d")
+        X = X.T();
     else
-        if isempty(opt.fmt)
-            opt.fmt = '%8.2g';
+        error("RVC3:printtform2d:badarg", "unknown type of pose")
+    end
+
+    % Print the label for this pose
+    %   label not given, use variable name if available
+    %   label="", show no label
+    %   label="label", show the given label
+    if ~isfield(options, "label")
+        options.label = string(inputname(1));
+    end
+
+    % Print the label for this pose
+    %   label not given, use variable name if available
+    %   label="", show no label
+    %   label="label", show the given label
+    if ~isfield(options, "label")
+        options.label = string(inputname(1));
+    end
+
+    if size(T,3) == 1
+        % scalar value
+        if options.fmt == ""
+            options.fmt = "%.3g";
+        end
+        s = tr2s2d(T, options);
+
+        if nargout == 0
+            fprintf(options.fid, s + "\n");
+        else
+            out = s;
+        end
+    else
+        % a sequence
+        if options.fmt == ""
+            options.fmt = "%8.2g";
         end
         
-        for i=1:size(X,3)
-            % for each matrix in a 3D array
-            s = char(s, tr2s2d(X(:,:,i), opt, args{:}) );
-        end
-    end
+        for i=1:size(T,3)
+            % for each SE(3) matrix in a 3D array
+            s = tr2s2d(T(:,:,i), options);
 
-    % if no output provided then display it
-    if nargout == 0
-        for row=s'
-            fprintf(opt.fid, '%s\n', row);
+            if nargout == 0
+                fprintf(options.fid, s+"\n");
+            end
         end
-    else
-        out = s;
     end
 end
 
-function s = tr2s2d(T, opt, varargin)
-    % print the translational part if it exists
-    if ~isempty(opt.label)
-        s = sprintf('%8s: ', opt.label);
+function s = tr2s2d(T, options)
+    % print the label is required
+    if options.label ~= ""
+        s = sprintf("%8s: ", options.label);
     else
-        s = '';
+        s = "";
     end
+
     if all(size(T) == [3 3])
-        % tform
-        s = [s, sprintf('t = (%s),', vec2s(opt.fmt, [T(1,3);T(2,3)]))];
+        % print the translational part if it exists
+        s = s + sprintf("t = (%s), ", vec2s(options.fmt, [T(1,3);T(2,3)]));
         R = T(1:2,1:2);
     else
         R = T;
@@ -114,25 +115,22 @@ function s = tr2s2d(T, opt, varargin)
     % print just the angle
     ang = atan2(R(2,1), R(1,1));
 
-    if strcmp(opt.unit, 'deg')
-        s = strcat(s, ...
-            sprintf(' %s deg', vec2s(opt.fmt, ang*180.0/pi)) );
+    if options.unit == "deg"
+        s = s + sprintf("%s deg", vec2s(options.fmt, rad2deg(ang)));
     else
-        s = strcat(s, ...
-            sprintf(' %s rad', vec2s(opt.fmt, ang)) );
+        s = s + sprintf("%s rad", vec2s(options.fmt, ang));
     end
 end
 
-
 function s = vec2s(fmt, v)
-    s = '';
+    % stringify a vector with specified format string and comma separated
+    % values
+    s = [];
     for i=1:length(v)
         if abs(v(i)) < 1e-12
             v(i) = 0;
         end
-        s = [s, sprintf(fmt, v(i))]; %#ok<AGROW>
-        if i ~= length(v)
-            s = [s, ', ']; %#ok<AGROW> % don't use strcat, removes trailing spaces
-        end
+        s = [s sprintf(fmt, v(i))]; %#ok<AGROW>
     end
+    s = join(s, ", ");
 end
