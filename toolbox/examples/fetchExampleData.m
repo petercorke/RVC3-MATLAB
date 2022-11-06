6b1b17fe46abb432770465df1810b2d37c733f01
thewitek <witekj@gmail.com>
2022-11-06 18:16:50 -0500

    Added code to fetch visual odometry example data and removed the data from Git to reduce the size of the toolbox.

--- toolbox/examples/fetchExampleData.m
+++ toolbox/examples/fetchExampleData.m
@@ -5,17 +5,13 @@ switch exampleName
 
     case "VisualOdometry"
 
-        disp("Fetching bridge-l.zip. This can take a while...")
-        url = "https://petercorke.com/files/images/bridge-l.zip";
+        fetchedFile = fetchFromWeb("bridge-l.zip");
+        outputFolder = fullfile("visodom", "left");
+        unpackFile(fetchedFile, outputFolder);
 
-        fetchedFile = "yo.zip";
-        websave(fetchedFile, url);
-
-        disp("Fetching bridge-r.zip. This can take a while...")
-        url = "https://petercorke.com/files/images/bridge-r.zip";
-
-        fetchedFile = "yo.zip";
-        websave(fetchedFile, url);
+        fetchedFile = fetchFromWeb("bridge-r.zip");
+        outputFolder = fullfile("visodom", "right");
+        unpackFile(fetchedFile, outputFolder);
 
     case "Mosaicking"
         url= "https://petercorke.com/files/images/mosaic.zip";
@@ -31,6 +27,29 @@ end
 
 end
 
-function fetchFomWeb(fetchThis)
+function fetchedFile = fetchFromWeb(fetchThisFile)
+
+rootUrlLocation = "https://petercorke.com/files/images/";
+
+disp("Fetching " + fetchThisFile +". This can take a while...")
+fileUrl = rootUrlLocation + fetchThisFile;
+disp("File was downloaded.")
+
+fetchedFile = fullfile(tempdir, fetchThisFile);
+
+websave(fetchedFile, fileUrl);
 
 end
+
+
+function unpackFile(fetchedFile, outputFolder)
+
+disp("Unpacking the ZIP file...")
+examplesRootFolder = fullfile(rvctoolboxroot, "examples");
+outputFullPathFolder = fullfile(examplesRootFolder, outputFolder);
+
+unzip(fetchedFile, outputFullPathFolder);
+disp("Done. Unpacked data is in " + outputFullPathFolder)
+
+end
+
